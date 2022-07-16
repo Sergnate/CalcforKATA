5e0d4209781dea8b721d17725c17693116cf4bc4
Sergnate <109349567+Sergnate@users.noreply.github.com>
2022-07-17 02:01:00 +0300

    Update RomNum.java

--- src/RomNum.java
+++ src/RomNum.java
@@ -1,74 +1,61 @@
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public enum RomNum {
-    I(1),
-    IV(4),
-    V(5),
-    IX(9),
-    X(10),
-    XL(40),
-    L(50),
-    XC(90),
-    C(100);
-
-    private int value;
-
-    private RomNum(int value) {
+    I(1), IV(4), V(5), IX(9), X(10),
+    XL(40), L(50), XC(90), C(100);
+    private final int value;
+    RomNum(int value) {
         this.value = value;
     }
-
     public int getValue() {
-        return this.value;
+        return value;
     }
-
     public static List getReverseSortedValues() {
-        return (List)Arrays.stream(values()).sorted(Comparator.comparing((e) -> {
-            return e.value;
-        }).reversed()).collect(Collectors.toList());
-    }
+        return Arrays.stream(values())
+                .sorted(Comparator.comparing((RomNum e) -> e.value).reversed())
+                .collect(Collectors.toList());
+    }public static String arabicToRoman(int number) {
+        if ((number <=0) || (number > 100)) {
+            throw new InputMismatchException("throws Exception //т.к. в римской системе нет отрицательных чисел"); }
 
-    public static String arabicToRoman(int number) {
-        if (number > 0 && number <= 100) {
-            List romanNumerals = getReverseSortedValues();
-            int i = 0;
-            StringBuilder sb = new StringBuilder();
+        List romanNumerals = RomNum.getReverseSortedValues();
 
-            while(number > 0 && i < romanNumerals.size()) {
-                RomNum currentSymbol = (RomNum)romanNumerals.get(i);
-                if (currentSymbol.getValue() <= number) {
-                    sb.append(currentSymbol.name());
-                    number -= currentSymbol.getValue();
-                } else {
-                    ++i;
-                }
-            }
+        int i = 0;
+        StringBuilder sb = new StringBuilder();
 
-            return sb.toString();
-        } else {
-            throw new IllegalArgumentException("throws Exception //т.к. в римской системе нет отрицательных чисел");
+        while ((number > 0) && (i < romanNumerals.size())) {
+            RomNum currentSymbol = (RomNum) romanNumerals.get(i);
+            if (currentSymbol.getValue() <= number) {
+                sb.append(currentSymbol.name());
+                number -= currentSymbol.getValue();
+            } else {
+                i++; }
         }
-    }
-
+        return sb.toString();}
     public static int romanToArabic(String input) {
         String romanNumeral = input.toUpperCase();
         int result = 0;
-        List romanNumerals = getReverseSortedValues();
+
+        List romanNumerals = RomNum.getReverseSortedValues();
+
         int i = 0;
 
-        while(romanNumeral.length() > 0 && i < romanNumerals.size()) {
-            RomNum symbol = (RomNum)romanNumerals.get(i);
+        while ((romanNumeral.length() > 0) && (i < romanNumerals.size())) {
+            RomNum symbol = (RomNum) romanNumerals.get(i);
             if (romanNumeral.startsWith(symbol.name())) {
                 result += symbol.getValue();
                 romanNumeral = romanNumeral.substring(symbol.name().length());
             } else {
-                ++i;
+                i++;
             }
         }
-        if (romanNumeral.length() > 0) {
+        if (romanNumeral.length() >0) {
             throw new IllegalArgumentException("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
-        } else { return result; }
-    }
+            }
+        return result;}
 }
