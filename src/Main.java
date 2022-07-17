33cf2cb91803d9c481bcfbd819a950aad6f4103e
Sergnate <109349567+Sergnate@users.noreply.github.com>
2022-07-17 04:56:35 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,34 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-    
+
     public static void main(String[] args) {
         Scanner scanstr = new Scanner(System.in);
         String input = scanstr.nextLine();
-        System.out.println(input.toUpperCase()+"="+Calc(input));}
+        if(input.length()>=3) {
+            System.out.println(input.toUpperCase()+"="+Calc(input));
+        } else throw new IllegalArgumentException("throws Exception //т.к. строка не является математической операцией");
+    }
     
-    public static String Calc(String input){
+    public static String Calc(String input) throws IllegalArgumentException {
         char[] znak = input.toCharArray();
-        String[] num = input.split("[-/+*]",2);
-        int n1, n2, rez = 0;
+        String[] num = input.split("[-/+*]",3);
+        if(num.length>2) {
+            throw new IllegalArgumentException("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+            }
+        if(num.length<=1) {throw new IllegalArgumentException("throws Exception //т.к. строка не является математической операцией");}
+        int n1,n2,rez = 0;
         boolean arabic=true;
         try {
             n1 = Integer.parseInt(num[0]);
             n2 = Integer.parseInt(num[1]);
-        } catch (NumberFormatException e) {arabic=false;
-            n1 = Integer.parseInt(String.valueOf(RomNum.romanToArabic(num[0])));
-            n2 = Integer.parseInt(String.valueOf(RomNum.romanToArabic(num[1])));}
-        if (n1 < 0 || n1 > 10 || n2 < 0 || n2 > 10) {
-            throw new InputMismatchException("throws Exception //т.к. формат математической операции не удовлетворяет заданию");
+        } catch (NumberFormatException e) {
+            arabic=false;
+            n1 = RomNum.romanToArabic(num[0]);
+            n2 = RomNum.romanToArabic(num[1]); }
+
+        if ((n1 < 0) || ((n1 > 10) || (n2 < 0)) || (n2 > 10)) {
+            throw new InputMismatchException("throws Exception //т.к. формат введенных чисел не удовлетворяет заданию");
         }
         for (int i = 0; i < input.length(); i++) {
             znak[i] = input.charAt(i);
@@ -40,10 +49,12 @@ public class Main {
                     } catch (ArithmeticException e) {
                         System.out.println("На ноль делить нельзя");
                     }
-                    break; }
-        }
+               break;
+
+                }
+            }
         if(!arabic) {
-            return RomNum.arabicToRoman(Integer.parseInt(String.valueOf(rez)));} else {return String.valueOf(rez);}
+            return RomNum.arabicToRoman(rez);} else {return String.valueOf(rez);}
     }
 }
 
